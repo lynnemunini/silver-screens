380a20d64b82c46cc59a125f003301a35cfe669b
lynnemunini <muninilynne65@gmail.com>
2022-11-09 12:52:41 +0300

    Making each row clickable

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.moviesapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -59,18 +61,24 @@ fun MainContent(movieList: List<String> = listOf(
     Column(modifier = Modifier.padding(12.dp)) {
         LazyColumn{
             items(items = movieList) {
-                MovieRow(movie = it)
+                MovieRow(movie = it){ movie ->
+                    Log.d("TAG", "MainContent: $movie")
+
+                }
             }
         }
     }
 }
 
 @Composable
-fun MovieRow(movie: String) {
+fun MovieRow(movie: String, onItemClick: (String) -> Unit = {}) {
     Card(modifier = Modifier
         .padding(4.dp)
         .fillMaxWidth()
-        .height(130.dp),
+        .height(130.dp)
+        .clickable{
+                  onItemClick(movie)
+        },
         shape = RoundedCornerShape(corner = CornerSize(16.dp)),
         elevation = 6.dp) {
         Row(verticalAlignment = Alignment.CenterVertically,
