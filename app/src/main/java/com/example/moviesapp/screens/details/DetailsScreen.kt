a4ce363011ef29e1f18238bd8f4568d9877b08b0
lynnemunini <muninilynne65@gmail.com>
2022-11-09 17:48:44 +0300

    Add navigating back on button clicked

--- app/src/main/java/com/example/moviesapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/moviesapp/screens/details/DetailsScreen.kt
@@ -1,15 +1,14 @@
 package com.example.moviesapp.screens.details
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -19,6 +18,12 @@ fun DetailsScreen(navController: NavController, movieData: String?) {
         .fillMaxHeight()) {
         Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
             Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
+            Spacer(modifier = Modifier.height(23.dp))
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text(text = "Go Back")
+            }
         }
     }
 }
